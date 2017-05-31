61c9b34cf8b7e1160cee4beea6626dbcb9206f7a
Sean Coates <sean@seancoates.com>
2017-05-31 12:51:21 -0400

    move to cookie/session based auth because apigw hates www-authenticate

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, Response, stream_with_context, request
+from flask import Flask, Response, stream_with_context, request, render_template, session, redirect, url_for
 import requests
 import boto3
 from botocore.credentials import Credentials as BotoCredentials
@@ -9,11 +9,13 @@ from aws_requests_auth.aws_auth import AWSRequestsAuth
 from base64 import b64decode
 import os
 from bcrypt import hashpw, gensalt
+import time
 
 
 logger = logging.getLogger()
 
 app = Flask(__name__, static_folder=None)
+app.secret_key = os.environ['SESSION_SECRET']  # intentionally not guarded so it fails, for now
 
 aws_default_region = os.environ.get("AWS_REGION", "us-east-1")
 
@@ -22,6 +24,7 @@ dynamo_table = os.environ.get("DYNAMODB_TABLE", "access_es_users")
 aws_region = os.environ.get("ES_REGION", aws_default_region)
 boto_session = boto3.session.Session(region_name=aws_region)
 credentials = boto_session.get_credentials().get_frozen_credentials()
+session_recheck = int(os.environ.get("SESSION_RECHECK", 300))  # recheck the session every 5 mins
 
 # from https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#hbh
 EXCLUDED_HEADERS = set([
@@ -43,54 +46,88 @@ EXCLUDED_HEADERS = set([
 dynamodb = boto3.resource('dynamodb', region_name=aws_region)
 table = dynamodb.Table(dynamo_table)
 
-methods = ['GET', 'POST', 'HEAD', 'PUT', 'OPTIONS', 'DELETE', 'PATH']
+
+def _check_login(username=None, password=None, expires=False):
+
+    if username is None and password is None:
+        # fetch from session
+        username = session.get('username')
+        password = session.get('password')
+        expires = session.get('expires')
+
+    if not username or not password:
+        return False
+
+    logger.info("Expires: {} {} {}".format(expires, time.time(), expires - time.time()))
+    if username and password and expires:
+        if expires > time.time():
+            logger.info('Unexpired session; skipping Dynamo')
+            return True
+
+
+    try:
+        response = table.query(KeyConditionExpression=Key('username').eq(username))
+
+        logger.debug("response: {}".format(response))
+
+        if response:
+            record = response.get('Items')
+            logger.debug("Got dynamodb record: {}".format(record))
+
+            if record:
+                this_record = record[0]
+
+                if this_record:
+                    passwd = this_record.get('password')
+                    logger.debug("Record has password: {}".format(passwd))
+                    if passwd:
+                        passwd = bytes(passwd, 'utf8')
+                        password = bytes(password, 'utf8')
+                        if passwd == hashpw(password, passwd):
+                            logger.debug("Password matches.")
+                            return True
+
+    # didn't find the record
+    except ClientError as e:
+        logger.debug("Did not get dynamodb record")
+        logger.debug(e)
+
+    return False
+
+
+@app.route('/--access-es-meta/login', methods=['GET'])
+def login_form():
+    return render_template('login.html', url=url_for('login_handler'))
+
+@app.route('/--access-es-meta/login', methods=['POST'])
+def login_handler():
+    if _check_login(request.form['username'], request.form['password']):
+        session['username'] = request.form['username']
+        session['password'] = request.form['password']
+        session['expires'] = time.time() + session_recheck
+        return redirect('/_plugin/kibana/app/kibana')
+    else:
+        return redirect(url_for('login_form'))
+
+
+@app.route('/--access-es-meta/logout', methods=['GET','POST'])
+def logout_handler():
+    session['username'] = None
+    session['password'] = None
+    return redirect(url_for('login_form'))
+
+
+methods = ['GET', 'POST', 'HEAD', 'PUT', 'OPTIONS', 'DELETE', 'PATCH']
 @app.route('/<path:path>', methods=methods)
 @app.route('/', defaults={'path': ''}, methods=methods)
 def proxy_request(path, **kwargs):
     """Takes an incoming request and proxies it to the specified host"""
 
-    successful_auth = False
-    auth_header = request.headers.get('authorization')
-    if auth_header:
-        logger.debug("got auth header")
-        (basic, auth) = auth_header.split(" ", 2)
-        if (basic.lower() == 'basic') and auth:
-
-            logger.debug("got basic")
-            (username, password) = b64decode(auth).split(b':', 2)
-            try:
-                logger.debug("query: {}".format(username.decode()))
-                response = table.query(KeyConditionExpression=Key('username').eq(username.decode()))
-
-                logger.debug("response: {}".format(response))
-
-                if response:
-                    record = response.get('Items')
-                    logger.debug("Got dynamodb record: {}".format(record))
-
-                    this_record = record[0]
-
-                    if this_record:
-                        passwd = this_record.get('password')
-                        logger.debug("Record has password: {}".format(passwd))
-                        if passwd:
-                            passwd = bytes(passwd, 'utf8')
-                            if passwd == hashpw(password, passwd):
-                                logger.debug("Password matches.")
-                                successful_auth = True
-
-            # didn't find the record
-            except ClientError as e:
-                logger.debug("Did not get dynamodb record")
-                logger.debug(e)
-                successful_auth = False
-
-    if not successful_auth:
-        return Response('You must authenticate.', 401, {'WWW-Authenticate':'Basic realm="Login Required"'})
+    if not _check_login():
+        return redirect(url_for('login_form'))
 
     url = request.url_rule.rule
     target_url = "https://{}/{}".format(remote_hostname, path)
-    logger.debug('target_url: {}'.format(target_url))
 
     method = getattr(requests, request.method.lower())
     headers = {
